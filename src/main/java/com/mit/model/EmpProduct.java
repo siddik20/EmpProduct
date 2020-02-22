72619d1933f335138b3f5bb1f44577ea16595ae3
Siddik <Siddik@DESKTOP-IQN22I3>
2020-02-22 17:36:02 +0530

    My Product m 7894

---
+++ src/main/java/com/mit/model/EmpProduct.java
@@ -0,0 +1,50 @@
+package com.mit.model;
+
+/**
+ * @author Siddik
+ *
+ */
+public class EmpProduct {
+	
+	private int eid;
+	private String eed;
+	private String eadr;
+	private String ecode;
+	
+	
+	public EmpProduct() {
+		super();
+	}
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEed() {
+		return eed;
+	}
+	public void setEed(String eed) {
+		this.eed = eed;
+	}
+	public String getEadr() {
+		return eadr;
+	}
+	public void setEadr(String eadr) {
+		this.eadr = eadr;
+	}
+	public String getEcode() {
+		return ecode;
+	}
+	public void setEcode(String ecode) {
+		this.ecode = ecode;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "EmpProduct [eid=" + eid + ", eed=" + eed + ", eadr=" + eadr + ", ecode=" + ecode + "]";
+	}
+
+	
+}
